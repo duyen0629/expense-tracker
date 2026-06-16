32c520ab9d9f44b5bd5ac84c3cb54b371d778727
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-16 11:23:42 +1000

    Create Screens and install Navigation

---
+++ screens/ManageExpense.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function AllExpenses() {
+    return (
+        <View>
+            <Text>Manage Expense</Text>
+        </View>
+    );
+}
+
+export default AllExpenses;
+
+const styles = StyleSheet.create({
+});
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function AllExpenses() {
+    return (
+        <View>
+            <Text>Recent Expenses</Text>
+        </View>
+    );
+}
+
+export default AllExpenses;
+
+const styles = StyleSheet.create({
+});
