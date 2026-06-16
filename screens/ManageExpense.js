4f44defdaf5cbd213b6d78eb41c8da6eb48491f0
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-16 13:54:12 +1000

    Support: edit expense mode and add expense mode

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,14 +1,23 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
-    return (
-        <View>
-            <Text>Manage Expense</Text>
-        </View>
-    );
+function ManageExpense({ route, navigation }) {
+  const expenseId = route.params?.expenseId;
+  const isEditing = !!expenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  return (
+    <View>
+      <Text>Manage Expense</Text>
+    </View>
+  );
 }
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
