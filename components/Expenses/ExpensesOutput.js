4e47e58e149af90b33c3e766529a6708531161df
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-16 12:03:38 +1000

    Update Expense Summary

--- components/Expenses/ExpensesOutput.js
+++ components/Expenses/ExpensesOutput.js
@@ -2,10 +2,10 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput(expenses) {
+function ExpensesOutput({ expenses, periodName }) {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} />
+      <ExpensesSummary periodName="Last 7 Days" expenses={expenses} />
       <ExpensesList expenses={expenses} />
     </View>
   );
