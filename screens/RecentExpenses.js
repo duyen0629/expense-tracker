1e1f97aac56322907b4232d0739574e33dfdcbdc
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-17 10:39:30 +1000

    Fetching data using GET Request

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -3,21 +3,26 @@ import { ExpensesContext } from "../store/expenses-context";
 import { useContext } from "react";
 import ExpensesOutput from "../components/Expenses/ExpensesOutput";
 import { getDateMinusDays } from "../util/date";
+import { useEffect } from "react";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const expensesContext = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
+
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
     const expenseDate = new Date(expense.date);
     return expenseDate.getTime() > getDateMinusDays(today, 7).getTime();
   });
-  return (
-    <ExpensesOutput
-      expenses={recentExpenses}
-      periodName="Last 7 Days"
-      fallbackText="No recent expenses found."
-    />
-  );
+  return <ExpensesOutput expenses={recentExpenses} periodName="Last 7 Days" fallbackText="No recent expenses found." />;
 }
 
 export default RecentExpenses;
--- util/http.js
+++ util/http.js
@@ -5,3 +5,17 @@ const BACKEND_URL = "https://chloe-react-native-default-rtdb.firebaseio.com/";
 export function storeExpense(expenseData) {
   axios.post(BACKEND_URL + "expenses.json", expenseData);
 }
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "expenses.json");
+  const expenses = [];
+  for (const key in response.data) {
+    expenses.push({
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    });
+  }
+  return expenses;
+}
